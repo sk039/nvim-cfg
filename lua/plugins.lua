553508ee07e6d1bdfc3d876da570be6de37b80ea
Ke Sun <sk.alvin.x@gmail.com>
2023-06-10 20:11:46 +0800

    update texlab config & luasnip keymap(TAB) config
    Signed-off-by: Ke Sun <sk.alvin.x@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -175,6 +175,16 @@ return require('packer').startup(function(use)
 
 			lspconfig.texlab.setup({
 				capabilities = capabilities,
+				settings = {
+					texlab = {
+						build = {
+							args = { "-xelatex", '-latexoption="--shell-escape"', "%f" },
+							executable = "latexmk",
+							forwardSearchAfter = false,
+							onSave = false
+						},
+					}
+				}
 			})
 
 			lspconfig.lua_ls.setup({
@@ -425,12 +435,13 @@ return require('packer').startup(function(use)
 		},
 		config = function ()
 			local cmp = require'cmp'
+			local luasnip = require'luasnip'
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
 						--vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
 						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 					end,
@@ -440,11 +451,29 @@ return require('packer').startup(function(use)
 					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
+					-- https://sbulav.github.io/vim/neovim-setting-up-luasnip/
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-n>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
-					['<TAB>'] = cmp.mapping.select_next_item(),
 					['<C-j>'] = cmp.mapping.select_next_item(),
 					['<C-k>'] = cmp.mapping.select_prev_item(),
 					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
